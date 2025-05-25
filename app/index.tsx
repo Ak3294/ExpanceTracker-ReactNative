94cd1f84ff413f62e00150885c723e6e3632d741
Akshay kumar hiran <akshaykumarhiran2@gmail.com>
2025-05-25 09:28:00 +0530

    FIRST commit

---
+++ app/index.tsx
@@ -0,0 +1,35 @@
+import { colors } from '@/constants/theme'
+import { useRouter } from 'expo-router'
+import React, { useEffect } from 'react'
+import { Image, StyleSheet, View } from 'react-native'
+
+const index = () => {
+  const router = useRouter()
+  useEffect(()=>{
+    setTimeout(()=>{
+      router.push('/(auth)/welcome')
+    },2000)
+  })
+  return (
+    <View style={styles.container}>
+      <Image resizeMode='contain' source={require('../assets/images/splashImage.png')} style={styles.container}/>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.neutral900
+  },
+
+  logo: {
+    width: '20%',
+    height: '20%',
+    aspectRatio: 1
+  }
+})
